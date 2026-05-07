0e500a2edfaec7e925a9c181924008e8f150137c
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-05-07 16:18:40 +0530

    project completed

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,38 +1,26 @@
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import cloudinary from "../config/cloudinary.js";
 
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async (req, file) => {
-        const isVideo = file.mimetype.startsWith("video");
-
-        return {
-            folder: "eCommerce_products",
-            resource_type: "auto",
-            format: isVideo ? undefined : "webp",
-            allowed_formats: [
-                "jpg",
-                "jpeg",
-                "png",
-                "webp",
-                "mp4",
-                "mkv",
-            ],
-            transformation: isVideo
-                ? []
-                : [
-                    { width: 1000, height: 1000, crop: "limit" },
-                    { quality: "auto" },
-                    { fetch_format: "auto" },
-                ],
-        };
-    },
+  cloudinary,
+  params: {
+    folder: "quickNest",
+    format: "webp",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    transformation: [
+      { width: 1000, height: 1000, crop: "limit" },
+      {
+        quality: "auto",
+      },
+      {
+        fetch_format: "auto",
+      },
+    ],
+  },
 });
 
-const uploads = multer({
-    storage,
-    limits: { fileSize: 50 * 1024 * 1024 },
-});
+const uploads = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 
 export default uploads;
